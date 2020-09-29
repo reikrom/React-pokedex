58bfa07fcf72ee72ed68c67a881f73c860d69b9c
Rei Krom <mr.mygo@gmail.com>
2020-09-29 22:10:09 +0100

    Updates

---
+++ precache-manifest.01db1e75c31a47809e3e7d97dd54e6c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcad446150c61cf5e2d25ae97a632fe",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "1aa8e626225bee1b5c44",
+    "url": "/react-pokedex/static/css/main.8c3ee9ad.chunk.css"
+  },
+  {
+    "revision": "595c6dfda147344b3487",
+    "url": "/react-pokedex/static/js/2.079fea68.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-pokedex/static/js/2.079fea68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa8e626225bee1b5c44",
+    "url": "/react-pokedex/static/js/main.0d50ebea.chunk.js"
+  },
+  {
+    "revision": "8d0dc51f96d551a6d0f3",
+    "url": "/react-pokedex/static/js/runtime-main.f27a760f.js"
+  },
+  {
+    "revision": "5114e127efd7818428f85708137f7e1a",
+    "url": "/react-pokedex/static/media/menusprites.5114e127.png"
+  }
+]);
